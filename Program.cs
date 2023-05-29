71cf5310a85d2c58fdbacdd4b81809e7d9df6137
shayan <shayan@UNKNOWN>
2023-05-29 13:57:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("enter the speed limit : ");
+        int speedLimit = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the speed of car:");
+        int carSpeed = int.Parse(Console.ReadLine());
+        if (carSpeed <= speedLimit)
+        {
+            Console.WriteLine("ok!");
+        }
+        else
+        {
+            int demeritPoints = (carSpeed - speedLimit) / 5;
+            Console.WriteLine("demerit points: { demeritPoints}");
+            if (demeritPoints > 12)
+            {
+                Console.WriteLine("license suspended!");
+
+            }
+        }
+
+    }
+}
